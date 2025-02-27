8820a48ad17aca70748fae878e72af27c29a73a0
pradeepkv012 <pradeepkv012@gmail.com>
2025-02-27 18:48:02 +0000

    updated score calculation logic

--- src/App.jsx
+++ src/App.jsx
@@ -7,26 +7,48 @@ function getRandomNum(min, max) {
 }
 
 let randomNums = []
-while (randomNums.length < 3) {
+let mines = 3
+while (randomNums.length < mines) {
   let randomNum = getRandomNum(1, 26)
   if (!randomNums.includes(randomNum)) {
     randomNums.push(randomNum)
   }
 }
-console.log(randomNums)
+console.log(randomNums.sort())
 
 function App() {
   
   const [GameOver, setGameOver] = useState(false)
   const [score, setScore] = useState(100)
+  const [tilesClicked, setTilesClicked] = useState(25)
+  const [safeTiles, setSafeTiles] = useState(25 - mines)
 
   let items = [];
   for (let index=1; index < 26; index++) {
     if (randomNums.includes(index)) {
-      items.push(<Square setScore={setScore} gameOver={GameOver} setGameOver={setGameOver} key={index} mine={true} />)
+      items.push(<Square 
+        setScore={setScore} 
+        gameOver={GameOver} 
+        setGameOver={setGameOver} 
+        key={index} 
+        isMine={true}
+        tilesClicked={tilesClicked}
+        setTilesClicked={setTilesClicked}
+        safeTiles={safeTiles}
+        setSafeTiles={setSafeTiles}
+        />)
     }
     else {
-      items.push(<Square setScore={setScore} gameOver={GameOver} setGameOver={setGameOver} key={index}/>)
+      items.push(<Square 
+        setScore={setScore} 
+        gameOver={GameOver} 
+        setGameOver={setGameOver} 
+        key={index}
+        tilesClicked={tilesClicked}
+        setTilesClicked={setTilesClicked}
+        safeTiles={safeTiles}
+        setSafeTiles={setSafeTiles}
+        />)
     }
   }
 
@@ -35,7 +57,7 @@ function App() {
     <div className='d-flex gap-10'>
       <div className='totalScore'>
         <p>Total Score</p>
-        <p>{score}</p>
+        <p>{score.toFixed(2)}</p>
       </div>
       <div className='d-grid'>
         {items}
